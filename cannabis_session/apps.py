0715050f6b2a7b2f38fcc01fbc30d476a8227a08
ayemereodia2 <odiadavid2@gmail.com>
2024-01-20 07:04:08 +0100

    initial set up

---
+++ cannabis_session/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CannabisSessionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cannabis_session'
---
+++ stash_manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StashManagerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stash_manager'
