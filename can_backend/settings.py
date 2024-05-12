5f179b38f23e71b42dd4f801129d0702008628f6
ayemereodia2 <odiadavid2@gmail.com>
2024-05-12 15:20:35 -0300

    updated test and api

--- can_backend/settings.py
+++ can_backend/settings.py
@@ -10,14 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from dotenv import load_dotenv
 import os
-from environ import Env
 from pathlib import Path
-
-
-env = Env()
-env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
